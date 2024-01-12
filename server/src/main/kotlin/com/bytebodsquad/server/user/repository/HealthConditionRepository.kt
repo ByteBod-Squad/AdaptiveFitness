d387c78841e79e5ca67027573753f77a31f811c0
Kevin Gida <kevingida@gmail.com>
2024-01-12 14:27:37 +0100

    Feat: add HealthConditionRepository

---
+++ server/src/main/kotlin/com/bytebodsquad/server/user/repository/HealthConditionRepository.kt
@@ -0,0 +1,10 @@
+package com.bytebodsquad.server.user.repository
+
+import com.bytebodsquad.server.user.entity.HealthCondition
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.UUID
+
+@Repository
+interface HealthConditionRepository : JpaRepository<HealthCondition, UUID> {
+}
