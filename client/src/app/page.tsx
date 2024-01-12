c0114d556df99f022824868194ac65abc79868cf
anondevmeister <degenprogrammer@gmail.com>
2024-01-12 10:21:58 +0100

    Style: responsiveness

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,7 +2,7 @@ import Header from './components/Header';
 import LandingPage from './components/landingPage';
 
 /* 
-@notice this is the first thing the user sees upon page load.
+@dev this is the first thing the user sees upon page load.
 Header height of 18% + section height of 82% should always add up to 100% (height of the users screen)
 */
 
