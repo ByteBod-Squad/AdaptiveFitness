defc05a7bfc90d707497abbb63f07c96ee94144d
Luke Williams <lukewilliams10101@gmail.com>
2024-01-16 09:41:01 +0100

    feat: implemented body area

--- server/src/main/kotlin/com/bytebodsquad/server/exercisegenerator/entity/BodyArea.kt
+++ server/src/main/kotlin/com/bytebodsquad/server/exercisegenerator/entity/BodyArea.kt
@@ -1,4 +1,15 @@
 package com.bytebodsquad.server.exercisegenerator.entity
 
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import org.hibernate.annotations.UuidGenerator
+import java.util.*
+
+@Entity
 class BodyArea {
+    @Id
+    @UuidGenerator
+    var id: UUID? = UUID.randomUUID()
+
+    var name: String = ""
 }
