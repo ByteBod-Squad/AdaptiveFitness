67e844c1decbd1237906aa3bc1268aa7c10cb79c
allanheremi <allanheremidev@gmail.com>
2024-01-17 09:59:25 +0100

    Feat: form layout

--- client/src/app/input/page.tsx
+++ client/src/app/input/page.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Header from '../components/Header';
+import Input from './components/Input';
 
 const page = () => {
-  return <div>Input route</div>;
+  return (
+    <main className="w-screen h-screen">
+      <Header />
+      <Input />
+    </main>
+  );
 };
 
 export default page;
