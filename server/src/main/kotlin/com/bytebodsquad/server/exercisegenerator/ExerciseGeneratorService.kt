81f1e3eb4d1dc2aa6ca86be0422aee2ab168e441
Kevin Gida <kevingida@gmail.com>
2024-01-17 10:34:41 +0100

    feat: add service for input data

--- server/src/main/kotlin/com/bytebodsquad/server/exercisegenerator/ExerciseGeneratorService.kt
+++ server/src/main/kotlin/com/bytebodsquad/server/exercisegenerator/ExerciseGeneratorService.kt
@@ -1,7 +1,17 @@
 package com.bytebodsquad.server.exercisegenerator
 
+import com.bytebodsquad.server.exercisegenerator.dto.InputDataResponse
+import com.bytebodsquad.server.exercisegenerator.entity.Equipment
+import com.bytebodsquad.server.exercisegenerator.repository.BodyAreaRepository
+import com.bytebodsquad.server.exercisegenerator.repository.EquipmentRepository
 import org.springframework.stereotype.Service
 
 @Service
-class ExerciseGeneratorService {
+class ExerciseGeneratorService (val bodyAreaRepository: BodyAreaRepository, val equipmentRepository: EquipmentRepository){
+
+    fun allInputData(): InputDataResponse {
+        val equipment = equipmentRepository.findAll()
+        val bodyArea = bodyAreaRepository.findAll()
+        return InputDataResponse(equipment,bodyArea)
+    }
 }
